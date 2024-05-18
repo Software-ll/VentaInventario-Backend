a13758daedf315ce469475755cd0ef0ffa08ddbf
julian andres arango <julianandresa8@gmail.com>
2024-05-18 12:10:52 -0500

    first commit

---
+++ src/main/java/com/ventaInventario/backend/VentaInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.ventaInventario.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventaInventario/backend/VentaInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventaInventario.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
